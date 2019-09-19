436a90c3d66da5c59bcc815490220f86b7db2cfd
Aida Nadine Gaita <gaitanadine@yahoo.com>
2019-09-19 21:01:29 +0300

    added controllers code
    codebase for the endpoints of the server

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '3a4d77fdfb7c42e4b7f0e1228df4befb'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+     .then(data => {
+         res.json(data)
+     })
+     .catch(err => res.status(400).json('unable to work with API'))    
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+     .increment('entries', 1)
+     .returning('entries')
+     .then(entries => {
+         res.json(entries[0]);
+     })
+    .catch(err => res.status(400).json('unable to get count'));
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+}
